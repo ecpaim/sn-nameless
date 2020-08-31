03bf249473b5cfc2175fe60b45da5b6bb994c947
eduardo <edupaim77@gmail.com>
2020-08-31 20:12:12 -0300

    added login, signup, jwt, validation and basic server protection

--- index.js
+++ index.js
@@ -1,11 +1,108 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express(); 
+const passport = require('passport');
+const helmet = require('helmet');
+const rateLimit = require("express-rate-limit");
+const router = express.Router(); // to add /api prefix
 
-app.get('/', (req,res) =>{
+const utils = require('./util/hash-and-token');
+const {validateSignupData, validateLoginData} = require('./util/validation');
+
+require('./config/passport')(passport);
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  });
+
+app.use(passport.initialize()); // initializes passport obj on every request
+app.use(express.json({ limit: '10kb' })); // reads requests as json, helps against DOS attacks
+app.use(helmet()); // protects against minor attacks
+app.use(limiter); // protects against brute force attacks
+app.use('/api', router); // needs to go after all middleware
+
+// just for tests
+const { users } = require('./db');
+console.log(users);
+
+// MAIN PAGE
+router.get('/', (req,res) =>{
     res.send('hello world')
-})
+});
 
+// LOGIN PAGE
+router.post('/login', (req,res) => {
+
+    // just checks if fields are not empty or bad written
+    const {errors, valid, email} = validateLoginData(req.body);
+    if(!valid){
+        return res.status(400).json({errors: errors, success: false});
+    }
+    let userExists;
+    if(email){
+        // will be replaced by a db call
+        userExists = users.filter(user => user.email === req.body.emailOrUser);
+    }
+    else{
+        // will be replaced by a db call
+        userExists = users.filter(user => user.username === req.body.emailOrUser);
+    }
+    
+  
+    if (userExists.length === 0) {
+        return res.status(401).json({ errors: { emailOrUser: 'Couldnt find email or username'}, success: false});
+    } else {
+
+        let user = userExists[0];
+        const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+
+        if(isValid){
+            const token = utils.issueJWT(user);
+            
+           return res.status(200).json({token: token, success: true});
+        } else {
+           return res.status(401).json({ errors: { password: 'Invalid password'}, success: false });
+        }
+    }
+
+});
+
+// SIGNUP PAGE
+router.post('/signup', (req,res) => {
+
+    const {errors, valid} = validateSignupData(req.body);
+
+    if(valid){
+        const {salt, hash} = utils.genPassword(req.body.password);
+        const newUser = {
+            _id: req.body.username,
+            username: req.body.username,
+            email: req.body.email,
+            hash: hash,
+            salt: salt
+        };
+        // this will be replaced by a db call
+        users.push(newUser);
+        if(true){
+            const token = utils.issueJWT(newUser)
+            console.log(newUser)
+            
+            return res.status(200).json({token: token, success: true});
+        } else {
+            return res.status(500).json({msg: 'couldnt add user to database', success: true});
+        }
+    } else {
+        return res.status(400).json({errors: errors, success: false});
+    }
+});
+
+// PROTECTED ROUTE, FOR TESTING PORPOUSES
+// we need {session: false} because jwt doesnt use sessions
+router.get('/protected', passport.authenticate('jwt', {session:false}), (req,res) =>{
+    
+    res.status(200).json({success: true, msg: 'you entered the protected route'})
+});
+
+port = 3000
 app.listen(port, () => {
-    console.log(`Listening on localhost:${port}`)
+    console.log(`Listening on localhost:${port}/api`)
 })
---
+++ util/validation.js
@@ -0,0 +1,71 @@
+/*
+    Generic form validation: check email,password and username fields for obvious sanitization and mistakes
+    Does not make any DB call
+*/
+const isValidEmail =(email) => {
+    //checks for email pattern
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(email.match(emailRegEx)) return true;
+    else return false;
+};
+// helper to see if string is empty
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+};
+const isValidUsername = (username) => {
+    const userRegex = "^[A-Za-z0-9_-]{3,}$";
+    if(username.match(userRegex)) return true;
+    else return false;
+}
+const isValidPassword = (password) => {
+    const userRegex = "^[A-Za-z0-9@#$%&*?]{6,}$";
+    if(password.match(userRegex)) return true;
+    else return false;
+}
+
+exports.validateSignupData = (data) => {
+    let errors = {};
+
+    if(!isValidEmail(data.email)) errors.email = 'Must be a valid email address';
+    if(isEmpty(data.email)) errors.email = 'Must not be empty';
+
+    if(!isValidUsername(data.username)) errors.username = 'Must have just numbers, letters and dashes';
+    if(data.username.trim().length < 3) errors.username = 'Must have at least 3 characters';
+
+    if(!isValidPassword(data.password)) errors.password = 'Must have just numbers, letters and @ # $ % & * ? as special characters';
+    if(data.password.trim().length < 6) errors.password = 'Must have at least 6 characters';
+
+    if(data.password !== data.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false
+    }
+};
+
+exports.validateLoginData = (data) => {
+    let errors = {};
+    let email;
+
+    if(isValidEmail(data.emailOrUser)){
+        email = true;
+    }
+    else if(isValidUsername(data.emailOrUser)) {
+        email = false;
+    }
+    else {
+        errors.emailOrUser = 'Invalid Email or Username';
+    }
+    if(isEmpty(data.emailOrUser)) errors.emailOrUser = 'Must not be empty';
+
+    if(!isValidPassword(data.password)) errors.emailOrUser = 'Invalid credentials'
+    if(isEmpty(data.password)) errors.password = 'Must not be empty';
+
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false,
+        email
+    }
+};
